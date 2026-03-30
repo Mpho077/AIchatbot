20982fc06e12ee7c9022fb1b1c5fd8f3c872a18b
Mpho077 <69971504+Mpho077@users.noreply.github.com>
2026-03-30 11:03:57 +0200

    Delete im_livechat_grok_ai directory

--- im_livechat_grok_ai/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import grok_api_endpoint
-from . import grok_ai_config
-from . import grok_ai_service
-from . import discuss_channel
-from . import im_livechat_channel
